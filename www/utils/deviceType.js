818142b13c1496bac84bc516772ba1fa7ecc7a16
chao.xu <chao.xu@l-001806.envisioncn.com>
2016-02-18 18:02:42 +0800

    删除www文件夹

--- www/utils/deviceType.js
+++
@@ -1,6 +0,0 @@
-export const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-export const isFileScheme = /file:/.test(location.protocol);
-export const isShell = isMobile && isFileScheme;
-export const isAndroid = /Android/i.test(navigator.userAgent);
-export const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-export const isBrowser = !isAndroid && !isiOS;
