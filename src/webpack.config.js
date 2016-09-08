a85520bdcddf47212b999a36fcb8ef7d3b476f85
Xu Chao <xuchao_zju@yeah.net>
2016-09-08 22:09:02 +0800

    dependency 更新

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       },
       {
         test: /\.jsx?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
-        loader: 'babel' // 加载模块 "babel" 是 "babel-loader" 的缩写
+        loader: 'babel', // 加载模块 "babel" 是 "babel-loader" 的缩写
+        exclude: /node_modules/
       },
       {
         test: /\.less$/,
